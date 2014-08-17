ab2060c5f76f5e59e86fc53a0faeaf35096a0de0
Frank Wöckener <fwoeck@gmail.com>
2014-08-17 11:51:52 +0200

    spawn three browsers

--- spec/spec-01.rb
+++ spec/spec-01.rb
@@ -3,26 +3,28 @@
 require './lib/setup'
 require './lib/helpers'
 
-NUM = ARGV[0] || 1
-
 module Session
   extend Capybara::DSL
   extend RSpec::Matchers
 
 
   def self.start
-    use_client(NUM)
+    use_client(100)
     visit_home_url
     login_as('100@mail.com')
+
+    use_client(101)
+    visit_home_url
+    login_as('101@mail.com')
+
+    use_client(103)
+    visit_home_url
+    login_as('103@mail.com')
   rescue => e
     puts e.message
-  end
-
-
-  def self.stop
+  ensure
     Capybara.reset_sessions!
   end
 end
 
 Session.start
-Session.stop
