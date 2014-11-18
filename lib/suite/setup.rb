160edc0d72d47d66c6b2647c01c89f1d8cd89e90
Frank Wöckener <fwoeck@gmail.com>
2014-11-18 17:03:25 +0000

    use chrome driver by default

--- lib/suite/setup.rb
+++ lib/suite/setup.rb
@@ -14,16 +14,23 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
-Capybara.register_driver :selenium_firefox_driver do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
-  profile['media.navigator.permission.disabled'] = true
-  profile.secure_ssl = false
-  profile.assume_untrusted_certificate_issuer = false
-  Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile)
+# Capybara.register_driver :selenium_firefox do |app|
+#   profile = Selenium::WebDriver::Firefox::Profile.new
+#   profile['media.navigator.permission.disabled'] = true
+#   profile.assume_untrusted_certificate_issuer = false
+#   profile.secure_ssl = false
+#
+#   Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile)
+# end
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, {browser: :chrome,
+    switches: %w[--ignore-certificate-errors --disable-translate --use-fake-device-for-media-stream --disable-user-media-security]
+  })
 end
 
 Capybara.app_host         = "https://#{SpecConfig['hostname']}"
 Capybara.run_server       =  false
 Capybara.server_port      =  443
 Capybara.default_selector = :css
-Capybara.default_driver   = :selenium_firefox_driver
+Capybara.default_driver   = :selenium_chrome # :selenium_firefox
